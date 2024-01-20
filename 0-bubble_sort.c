9621dbe2f6d1dc8a91a893862f701db6e9ace215
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-01-20 18:06:51 +0100

    task one done

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,6 +10,7 @@
 void bubble_sort(int *array, size_t size)
 {
 size_t i, j;
+int temp;
 int swapped;
 
 if (array == NULL || size < 2)
@@ -22,7 +23,9 @@ for (j = 0; j < size - 1 - i; j++)
 {
 if (array[j] > array[j + 1])
 {
-swap(&array[j], &array[j + 1]);
+temp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = temp;
 swapped = 1;
 print_array(array, size);
 }
