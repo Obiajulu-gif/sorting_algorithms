5545d21c5927b401fba87357926285698d0ce348
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-01-23 02:42:13 +0100

    task 102 done

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,48 +1,45 @@
 #include "sort.h"
 
 /**
- * counting_sort - Sorts an array of integers in ascending order
- *                  using sort algorithm
- * @array: the array to be sorted
- * @size: the size of the array
+ * counting_sort - Sorts an array of integers in ascending order using
+ *                 Counting Sort algorithm.
+ * @array: The array to be sorted
+ * @size: The size of the array
  */
 void counting_sort(int *array, size_t size)
 {
-int i, j, max;
-size_t i = 0;
-int *count;
+    size_t i = 0;
+    int x, j;
+    int max = array[0];
+    int *count;
+    if (array == NULL || size < 2)
+        return;
 
-if (array == NULL || size < 2)
-return;
+    for (i = 1; i < size; i++)
+    {
+        if (array[i] > max)
+            max = array[i];
+    }
 
-max = array[0];
-for (size_t i = 0; i < size; i++)
-{
-if (array[i] > max)
-max = array[i];
-}
+    count = malloc((max + 1) * sizeof(int));
+    if (count == NULL)
+        return;
 
-count = malloc((max + 1) * sizeof(int));
-if (count == NULL)
-return;
+    for (x= 0; x <= max; i++)
+        count[x] = 0;
 
-for (i = 0; i <= max; i++)
-count[i] = 0;
+    for (i = 0; i < size; i++)
+        count[array[i]]++;
 
-for (size_t i = 0; i < size; i++)
-count[array[i]]++;
+    print_array(count, max + 1);
 
-print_array(count, max + 1);
-
-for (j = 0; j <= max; j++)
-{
-while (count[j] > 0)
-{
-array[i++] = j;
-count[j]--;
-}
-
-}
-
-free(count);
+    for (x = 0; x <= max; x++)
+    {
+        while (count[x] > 0)
+        {
+            array[j++] = i;
+            count[x]--;
+        }
+    }
+    free(count);
 }
